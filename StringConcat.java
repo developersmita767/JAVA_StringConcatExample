1515875904070e7d94bae11379b72ba18933cfef
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-04-21 12:06:40 +0530

    Add files via upload

---
+++ StringConcat.java
@@ -0,0 +1,9 @@
+public class StringConcat{
+    public static void main(String[] ar){
+        System.out.println("Hello"+2);
+        System.out.println("Hello"+2+3);
+        System.out.println("Hello"+2*3);
+        int a=10;
+        System.out.println(a++ * 2);
+    }
+}
